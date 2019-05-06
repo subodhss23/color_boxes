e62ceb94e4f7d26379e52de936c952a782917ef8
Subodh Sharma <subodhss23@gmail.com>
2019-05-05 20:53:09 -0700

    adding src

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Box from './Box';
+import BoxContainer from './BoxContainer';
+
+class App extends Component{
+    render(){
+        return (
+          <div className="App">
+            <BoxContainer />
+          </div>
+        );
+    }
+}
+
+export default App;
